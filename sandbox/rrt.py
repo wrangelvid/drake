f99c3704702721a6f7d2a0502223f2e6c1685877
Peter <wernerpe@mit.edu>
2021-11-29 10:01:01 -0500

    rrt cleanup

--- sandbox/rrt.py
+++ sandbox/rrt.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 class Node:
     def __init__(self, pos, parent = None):
@@ -20,7 +18,8 @@ class RRT:
                  extend_steps = 1e-2, 
                  init_goal_sample_rate = 0.05,
                  goal_sample_rate_scaler = 0.1,
-                 verbose = False
+                 verbose = False,
+                 plotcallback = None
                  ):
 
         #col_check(pos) == True -> in collision!
@@ -53,16 +52,13 @@ class RRT:
         self.extend_step_size = extend_steps
         self.max_extend_steps = int(max_extend_length/self.extend_step_size)
         self.verbose = verbose
-        
+        self.plotcallback = plotcallback
+        self.do_plot = True if self.plotcallback is not None else False
+
         self.dim = len(start)
         self.distance_to_go = 1e9
 
-        self.fig, self.ax = plt.subplots(nrows = 1, ncols = 1)
-        self.ax.set_xlim([-1, 1])
-        self.ax.set_ylim([-1, 1])
-        self.ax.scatter(root.pos[0], root.pos[1], c = 'r', s = 50)
-        self.ax.scatter(goal[0], goal[1], c = 'g', s = 50)
-        plt.show(block=False)
+       
 
     def get_closest_node(self, pos):
         dists = np.linalg.norm(pos.reshape(-1,2) - np.array(self.node_pos), axis=1)
@@ -129,7 +125,6 @@ class RRT:
 
         for it in range(n_it):
             pos_samp = self.sample_node_pos()
-            self.ax.scatter(pos_samp[0], pos_samp[1], c = 'k', s = 5, alpha= 1.0)
             nearest_id = self.get_closest_node(pos_samp) 
             parent_node = self.nodes[nearest_id]
             child_node = self.grow_naive(parent_node, pos_samp)
@@ -138,14 +133,15 @@ class RRT:
             self.node_pos.append(child_node.pos)
             dist_to_target = np.linalg.norm(self.goal - self.node_pos[-1])
 
-            self.ax.plot([child_node.pos[0], parent_node.pos[0]], [child_node.pos[1], parent_node.pos[1]], c = 'k')
-            self.ax.scatter(child_node.pos[0], child_node.pos[1], c = 'k', s = 5, alpha= 1.0)
+            if self.do_plot:
+                self.plotcallback(parent_node, child_node, pos_samp)
+
             if dist_to_target<self.distance_to_go:
                 self.distance_to_go = dist_to_target
                 self.closest_id = child_node.id
-                if self.verbose:
-                    print("it: {iter} distance to target {dist: .3f}".format(iter =it, dist = self.distance_to_go))
                 self.goal_sample_rate = np.clip(0.8 - self.distance_to_go*self.goal_sample_rate_scaler, a_min = self.init_goal_sample_rate, a_max = 1)
+                if self.verbose:
+                    print("it: {iter} distance to target: {dist: .3f} goalsample prob: {prob: .3f}".format(iter =it, dist = self.distance_to_go, prob = self.goal_sample_rate))
 
             if self.distance_to_go< self.extend_step_size:
                 break
@@ -154,12 +150,6 @@ class RRT:
         path = []
         current_node = self.nodes[self.closest_id]
         while current_node.parent is not None:
-
-            self.ax.scatter(current_node.pos[0], current_node.pos[1], c = 'r', s = 10)
-            
-            xs = [current_node.pos[0], current_node.parent.pos[0]]
-            ys = [current_node.pos[1], current_node.parent.pos[1]]
-            self.ax.plot(xs, ys, c = 'r')
             current_node = current_node.parent
             path.append(current_node.pos)
         
