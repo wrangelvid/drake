a55ac6314c6287a5d7628f1e75182ecfe24c7f11
Peter <wernerpe@mit.edu>
2021-12-02 22:59:28 -0500

    prm

--- sandbox/prm.py
+++ sandbox/prm.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial import cKDTree
+from scipy.sparse.csgraph import dijkstra
 
 class Node:
     def __init__(self, pos, cost, parent = None):
@@ -41,13 +42,13 @@ class PRM:
             nodes.append(self.sample_node_pos())
             if self.verbose and idx%30 == 0:
                 print('[PRM] Samples', idx)
+        self.nodes_list = nodes
         self.nodes = np.array(nodes)
         self.nodes_kd = cKDTree(self.nodes)   
         
         #generate edges
-        self.adjacency_list = self.connect_nodes()
-        if self.plotcallback:
-            self.plotcallback(self.nodes, self.adjacency_list)
+        self.adjacency_list, self.dist_adj = self.connect_nodes()
+        self.plot()
 
 
     def sample_node_pos(self, collision_free = True, MAXIT = 1e4):
@@ -73,10 +74,12 @@ class PRM:
    
     def connect_nodes(self,):
         adjacency_list = []
+        dist_adj = []
         for node_idx in range(self.nodes.shape[0]):
             if self.verbose and node_idx%20 == 0:
                 print('[PRM] Nodes connected:', node_idx)
             edges = []
+            edge_dist = []
             dists, idxs = self.nodes_kd.query(self.nodes[node_idx, :], k= self.num_neighbours, p= 2, distance_upper_bound = self.dist_thresh ) 
             #linesearch connection for collision
             for step in range(len(idxs)):
@@ -90,9 +93,63 @@ class PRM:
                             break
                     if add:
                         edges.append(nearest_idx)
-
+                        edge_dist.append(dists[step])
             adjacency_list.append(edges)
-        return adjacency_list
+            dist_adj.append(edge_dist)
+        return adjacency_list, dist_adj
 
+    def add_start_end(self, start, end):
+        self.nodes_list.append(start)
+        self.nodes_list.append(end)
+        self.nodes = np.array(self.nodes_list)
+        self.nodes_kd = cKDTree(self.nodes) 
+        for node_idx in [-2, -1]:
+            edges = []
+            edge_dist = []
+            dists, idxs = self.nodes_kd.query(self.nodes[node_idx, :], k= self.num_neighbours, p= 2, distance_upper_bound = self.dist_thresh ) 
+            #linesearch connection for collision
+            for step in range(len(idxs)):
+                nearest_idx =idxs[step]
+                if not dists[step] == np.inf:
+                    add = True
+                    for t in self.t_check:
+                        pos = (1-t)*self.nodes[node_idx, :] + t*self.nodes[nearest_idx, :]
+                        if self.in_collision(pos):
+                            add = False
+                            break
+                    if add:
+                        edges.append(nearest_idx)
+                        edge_dist.append(dists[step])
+            self.adjacency_list.append(edges)
+            self.dist_adj.append(edge_dist)
 
+    def build_adjacency_mat(self,):
+        N = len(self.nodes)
+        ad_mat = np.zeros((N, N))
+        for idx in range(N):
+            nei_idx = 0
+            for nei in self.adjacency_list[idx]:
+                if not nei == idx:
+                    ad_mat[idx, nei] = self.dist_adj[idx][nei_idx]
+                    ad_mat[nei, idx] = self.dist_adj[idx][nei_idx]
+                     
+                nei_idx +=1
+        return ad_mat
     
+    def find_shortest_path(self):
+        ad_mat = self.build_adjacency_mat()
+        dist, pred = dijkstra(ad_mat, directed=False, indices=-2, return_predecessors=True)
+        sp_list = []
+        sp_length = dist[-1]
+        current_idx = -1
+        sp_list.append(self.nodes[current_idx])
+        while not current_idx == ad_mat.shape[0]-2:
+            current_idx = pred[current_idx]
+            sp_list.append(self.nodes[current_idx])
+        return sp_list, sp_length
+
+    def plot(self,):
+        if self.plotcallback:
+            self.plotcallback(self.nodes, self.adjacency_list)
+        else:
+            pass
